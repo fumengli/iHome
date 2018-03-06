f0cd8c68d84c5946563c0bb2f0c90b00c708046a
wbcg00 <evenio@163.com>
2018-03-06 09:46:00 +0800

    增加用户登录操作

---
+++ controllers/logIn_user_controller.go
@@ -0,0 +1,73 @@
+package controllers
+
+//api/v1.0/userss
+//LoginNewUser
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"iHome/models"
+)
+
+type LoginUserController struct {
+	beego.Controller
+}
+
+func (this *LoginUserController) RetrunData(retmsg interface{}) {
+	this.Data["json"] = retmsg
+	this.ServeJSON()
+}
+func (this *LoginUserController) LoginUser() {
+	beego.Info("=========GetLoginInfo succ!=============")
+	var retmsg = make(map[string]interface{})
+	retmsg["errno"] = models.RECODE_SESSIONERR
+	retmsg["errmsg"] = models.RecodeText(models.RECODE_SESSIONERR)
+	defer this.RetrunData(retmsg)
+	//1 得到客户端请求的json数据
+	var regRequestMap = make(map[string]interface{})
+	json.Unmarshal(this.Ctx.Input.RequestBody, &regRequestMap)
+
+	//beego.Info("mobile= ", regRequestMap["mobile"])
+	//beego.Info("password=", regRequestMap["password"])
+	//beego.Info("sms_code=", regRequestMap["sms_code"])
+	//2 判断用户数据合法性
+	if regRequestMap["mobile"] == "" || regRequestMap["password"] == "" {
+		retmsg["errno"] = models.RECODE_PARAMERR
+		retmsg["errmsg"] = models.RecodeText(models.RECODE_PARAMERR)
+		return
+	}
+	//3 从mysql中查询数据
+	user := models.User{Name: regRequestMap["mobile"].(string)}
+
+	o := orm.NewOrm()
+	err := o.Read(&user, "Name")
+	if err != nil {
+		beego.Info("Read error = ", err)
+		retmsg["errno"] = models.RECODE_NODATA
+		retmsg["errmsg"] = models.RecodeText(models.RECODE_NODATA)
+		return
+	} //有该用户
+	beego.Info("==========read succ===========", user.Name)
+	md5Ctx := md5.New()
+	md5Ctx.Write([]byte(regRequestMap["password"].(string)))
+	input_hash := hex.EncodeToString(md5Ctx.Sum(nil))
+	if user.Password_hash == input_hash { // password ok
+		retmsg["errno"] = models.RECODE_OK
+		retmsg["errmsg"] = models.RecodeText(models.RECODE_PWDERR)
+	} else {
+		beego.Info("===========password err=============")
+		beego.Info("db_hash", user.Password_hash)
+		beego.Info("input_hash", input_hash)
+		retmsg["errno"] = models.RECODE_PWDERR
+		retmsg["errmsg"] = models.RecodeText(models.RECODE_PWDERR)
+		return
+	}
+	//4 将当前的用户的信息存储到session中
+	this.SetSession("name", user.Mobile)
+	this.SetSession("user_id", user.Id)
+	this.SetSession("mobile", user.Mobile)
+	return
+}
