6a40ca14e4bc2395cbb0a6ffbb78b22e31e80982
wbcg00 <evenio@163.com>
2018-03-06 09:43:27 +0800

    缓存动作,添加,读取,删除

---
+++ controllers/action_session_controller.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+	_ "github.com/astaxie/beego/orm"
+	"iHome/models"
+)
+
+type SessionController struct {
+	beego.Controller
+}
+
+func (this *SessionController) RetrunData(retmsg interface{}) {
+	this.Data["json"] = retmsg
+	this.ServeJSON()
+}
+
+func (this *SessionController) GetSessionName() {
+	beego.Info("=========GetSession succ!=============")
+	var retmsg = make(map[string]interface{})
+	retmsg["errno"] = models.RECODE_SESSIONERR
+	retmsg["errmsg"] = models.RecodeText(models.RECODE_SESSIONERR)
+	defer this.RetrunData(retmsg)
+	nameMap := make(map[string]interface{})
+	name := this.GetSession("name")
+	if name != nil {
+		retmsg["errno"] = models.RECODE_OK
+		retmsg["errmsg"] = models.RecodeText(models.RECODE_OK)
+		nameMap["name"] = name
+		retmsg["data"] = nameMap
+	}
+	return
+}
+
+func (this *SessionController) DelSessionName() {
+	beego.Info("=========DelSession succ!=============")
+	var retmsg = make(map[string]interface{})
+	retmsg["errno"] = models.RECODE_OK
+	retmsg["errmsg"] = models.RecodeText(models.RECODE_OK)
+	defer this.RetrunData(retmsg)
+	this.DelSession("name")
+	this.DelSession("user_id")
+	this.DelSession("mobile")
+	return
+
+}
