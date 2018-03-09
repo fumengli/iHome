39c870e18d58ddbe2fbd8746a67e558cd6c426ef
wbcg00 <evenio@163.com>
2018-03-09 10:54:08 +0800

    加载用户

--- controllers/house_load.go
+++ controllers/house_load.go
@@ -1,8 +1,9 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
-	_ "github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/orm"
 	"iHome/models"
 )
 
@@ -18,21 +19,25 @@ func (this *HouseLoadController) RetrunData(retmsg interface{}) {
 func (this *HouseLoadController) GetHouseLoad() {
 	beego.Info("=========GetHouseLoad succ!=============")
 	var retmsg = make(map[string]interface{})
+	retmsg["errno"] = models.RECODE_SESSIONERR
+	retmsg["errmsg"] = models.RecodeText(models.RECODE_SESSIONERR)
+	defer this.RetrunData(retmsg)
+	//1 通过session 拿到用户的user_id
+	userId := this.GetSession("user_id")
+	if userId == nil {
+		beego.Info("userid session is empty")
+		return
+	}
+	//根据id 查询该用户信息
+	o := orm.NewOrm()
+	var houses []models.House = []models.House{}
+	num, err := o.QueryTable("house").Filter("user", userId.(int)).All(&houses)
+	if err == nil {
+		fmt.Printf("%d houses read\n", num)
+	}
+	//组织返回数据
 	retmsg["errno"] = models.RECODE_OK
 	retmsg["errmsg"] = models.RecodeText(models.RECODE_OK)
-	defer this.RetrunData(retmsg)
-	/*
-		if ret, err := o.QueryTable("area").All(&areas); err != nil {
-			retmsg["errno"] = models.RECODE_DBERR
-			retmsg["errmsg"] = models.RecodeText(models.RECODE_DBERR)
-			return
-		} else if ret == 0 {
-			retmsg["errno"] = models.RECODE_NODATA
-			retmsg["errmsg"] = models.RecodeText(models.RECODE_NODATA)
-			return
-		}
-		//succ
-		retmsg["data"] = areas
-		return
-	*/
+	retmsg["data"] = houses
+	return
 }
